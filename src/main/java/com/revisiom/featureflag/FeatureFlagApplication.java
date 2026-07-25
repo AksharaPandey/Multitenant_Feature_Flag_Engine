68115230c9d7a2bbb792a183b30b6122cdb4e96a
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-25 13:59:31 +0530

    Enable caching

--- src/main/java/com/revisiom/featureflag/FeatureFlagApplication.java
+++ src/main/java/com/revisiom/featureflag/FeatureFlagApplication.java
@@ -3,8 +3,10 @@ package com.revisiom.featureflag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FeatureFlagApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeatureFlagApplication.class,args);
