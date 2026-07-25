5a41e7e45b2f6a1202b1812c197128c3e3124eef
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-25 12:42:35 +0530

    Custom Annotations

--- src/main/java/com/revisiom/featureflag/FeatureFlagApplication.java
+++ src/main/java/com/revisiom/featureflag/FeatureFlagApplication.java
@@ -1,4 +1,12 @@
 package com.revisiom.featureflag;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FeatureFlagApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureFlagApplication.class,args);
+    }
 }
