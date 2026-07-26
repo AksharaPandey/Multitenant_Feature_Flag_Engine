d841156130079cc070285572e1f522564cd2b820
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-26 14:20:39 +0530

    Flags dynamic

--- src/main/java/com/revisiom/featureflag/strategy/SimpleToggleEvaluator.java
+++ src/main/java/com/revisiom/featureflag/strategy/SimpleToggleEvaluator.java
@@ -1,19 +1,33 @@
 package com.revisiom.featureflag.strategy;
 
-
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 public class SimpleToggleEvaluator implements FeatureEvaluator {
-    private final Map<String,Boolean> featureFlags=Map.of(
+
+    private final Map<String, Boolean> featureFlags = new ConcurrentHashMap<>(Map.of(
             "NEW_PAYMENT_GATEWAY", true,
             "BETA_RECOMMENDATIONS", false
-    );
+    ));
+
     @Override
     @Cacheable(value = "featureFlags", key = "#featureKey")
-    public boolean isEnabled(String featureKey){
-        System.out.println("Fetching feature status from source for: " + featureKey);
-        return featureFlags.getOrDefault(featureKey,false);
+    public boolean isEnabled(String featureKey) {
+        return featureFlags.getOrDefault(featureKey, false);
+    }
+
+    public Map<String, Boolean> getAllFlags() {
+        return featureFlags;
+    }
+
+    // Clears cache when a flag status changes so AOP gets the updated value immediately
+    @CacheEvict(value = "featureFlags", key = "#featureKey")
+    public void toggleFlag(String featureKey, boolean isEnabled) {
+        featureFlags.put(featureKey, isEnabled);
     }
 }
